544813a37732e75a30f27822e0afbb1183fa21c5
dabowbow <dabowbow@gmail.com>
2016-12-26 22:45:06 +0800

    create groups index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'welcome#index'
+  resources :groups
+
+  #root 'welcome#index'
+  root 'groups#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
